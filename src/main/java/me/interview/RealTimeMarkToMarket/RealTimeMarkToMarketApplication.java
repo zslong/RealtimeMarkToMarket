61b0d2aa2c8fab4b6fc5f5411f4afb85bf96dcc4
zslong <z.s-long@163.com>
2023-01-02 15:15:01 +0800

    first commit

---
+++ src/main/java/me/interview/RealTimeMarkToMarket/RealTimeMarkToMarketApplication.java
@@ -0,0 +1,13 @@
+package me.interview.RealTimeMarkToMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeMarkToMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeMarkToMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/interview/RealTimeMarkToMarket/RealTimeMarkToMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package me.interview.RealTimeMarkToMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeMarkToMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
